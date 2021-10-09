663d4d52e3d6b6763cb59b5da0ba8fa3880f9781
imnotaplayer <91289951+imnotaplayer@users.noreply.github.com>
2021-10-09 19:35:32 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,159 @@
+local time = os.time()
+
+math.randomseed(time) -- for a random result
+
+local d = {}
+local list = {}
+
+-- get the current list
+local f = io.open("Remaining-List.txt", "rb")
+if f then
+    for line in f:lines() do
+        if line:len() > 1 then
+            table.insert(d, line)
+        end
+    end
+    f:close()
+else
+    print("Created Remaining-List.txt. The List will be empty.")
+    -- this is a bit questionable.
+    -- i believe it repeatedly overwrite on a write-only system.
+    local f = io.open("Remaining-List.txt", "w")
+    if f then
+        f:write("\n")
+        f:close()
+    else
+        print("Failed to created Remaining-List.txt")
+    end
+end
+local f = io.open("The-List.txt", "rb")
+if f then
+    for line in f:lines() do
+        if line:len() > 1 then
+            table.insert(list, line)
+        end
+    end
+    f:close()
+else
+    print("Created The-List.txt")
+    -- same here.
+    local f = io.open("The-List.txt", "w")
+    if f then
+        f:write("\n")
+        f:close()
+    else
+        print("Failed to created The-List.txt")
+    end
+end
+
+f = nil
+
+-- info message
+if #d == 0 then
+    print('New levels can be added with the "add <level>" command or by adding to Remaining-List.txt and adding each level on it\'s '..
+          'own line and restarting the application.')
+    print('Levels can also be removed with the "remove <level>" command or by removing from Remaining-List.txt')
+    print('You can change your current list of levels to your chosen levels with "swap"\n'..
+          'If you want to keep a backup of your list, I suggest making one before using this command, this isn\'t reverseable.')
+end
+
+-- saves the list and remaining levels to files
+local function save()
+    do
+        local s = table.concat(d, "\n")
+        local f = io.open("Remaining-List.txt", "wb")
+        f:write(s, "\n")
+        f:close()
+    end
+    do
+        local s = table.concat(list, "\n")
+        local f = io.open("The-List.txt", "wb")
+        f:write(s, "\n")
+        f:close()
+    end
+end
+
+-- main loop
+while true do
+    local i = io.read()
+    if i == "" or i == "next" or i == "continue" then
+        -- <d> is the active list that levels will be chosen from
+        -- <list> is the list of levels that will be swapped to with the "swap" command
+        if #d == 0 then
+            print("There are no levels left in The List.")
+        else
+            local c = math.random(1, #d)
+            local n = d[c]
+            print(n)
+            io.write("Add to The List? (y/n) ")
+            local i = io.read():lower()
+            if i:sub(1, 1) == "y" then
+                table.insert(list, n)
+                print(#list.." "..(#list == 1 and "entry" or "entries").." now on The List.")
+            else
+                print("Skipped level.")
+            end
+            table.remove(d, c)
+            save()
+        end
+    elseif i == "list" then
+        save()
+        if #list == 0 then
+            print("The List: (..empty..)")
+        else
+            print("The List: "..s)
+            print("Remaining: "..table.concat(d))
+        end
+    elseif i:sub(1, 4) == "add " then
+        local level = i:sub(5, -1)
+        local valid = true
+        for k, v in ipairs(d) do
+            if v:lower() == level:lower() then
+                valid = false
+                break
+            end
+        end
+        if valid then
+            for k, v in ipairs(list) do
+                if v:lower() == level:lower() then
+                    valid = false
+                    break
+                end
+            end
+        end
+        if valid then
+            table.insert(d, level)
+            print("Added "..level.." to current list.")
+            save()
+        else
+            print("Level already accounted for.")
+        end
+    elseif i:sub(1, 7) == "remove " then
+        local level = i:sub(8, -1)
+        local valid = false
+        for k, v in ipairs(d) do
+            if v:lower() == level:lower() then
+                valid = k
+                break
+            end
+        end
+        if valid then
+            local level = table.remove(d, valid)
+            print("Removed "..level.." from current list.")
+            save()
+        else
+            print("Level not in list.")
+        end
+    elseif i == "swap" then
+        d = list
+        list = {}
+        -- is this misleading?
+        print("Swapped list with saved list.")
+        -- save()
+    elseif i == "exit" or i == "quit" then
+        -- unlike breaking with CTRL+C, this will save before exiting,
+        -- which may or may not be desirable
+        save()
+        os.exit()
+    end
+end
